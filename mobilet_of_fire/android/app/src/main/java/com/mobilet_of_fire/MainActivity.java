58ef7dc00da11e99e7015382334d80fdc2624fe1
Aylee <ayleeandersen@gmail.com>
2019-03-21 10:19:29 -0600

    Added react-native project with react navigation

---
+++ mobilet_of_fire/android/app/src/main/java/com/mobilet_of_fire/MainActivity.java
@@ -0,0 +1,28 @@
+package com.mobilet_of_fire;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "mobilet_of_fire";
+    }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+            @Override
+            protected ReactRootView createRootView() {
+                return new RNGestureHandlerEnabledRootView(MainActivity.this);
+            }
+        };
+    }
+}
