c02b0dc53363bdaa5615dcfe1e002cb343448053
Aylee <ayleeandersen@gmail.com>
2019-03-22 13:38:44 -0600

    Added navigation root

--- mobilet_of_fire/src/App.js
+++ mobilet_of_fire/src/App.js
@@ -8,26 +8,28 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+import HomeScreen from './screens/HomeScreen';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+        <AppContainer />
     );
   }
 }
+
+const Root = createStackNavigator(
+  {
+      Home: HomeScreen,
+  },
+  {
+      initialRouteName: 'Home'
+  }
+);
+
+const AppContainer = createAppContainer(Root);
 
 const styles = StyleSheet.create({
   container: {
