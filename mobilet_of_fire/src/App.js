46def259977bcb214f3a10ff5f63202448d04577
Aylee <ayleeandersen@gmail.com>
2019-03-25 13:50:52 -0600

    Changed navigator

--- mobilet_of_fire/src/App.js
+++ mobilet_of_fire/src/App.js
@@ -8,7 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import { createAppContainer, createStackNavigator } from 'react-navigation';
+import { createAppContainer, createStackNavigator, createMaterialTopTabNavigator } from 'react-navigation';
 import HomeScreen from './screens/HomeScreen';
 import Favorites from './screens/Favorites';
 import Search from './screens/Search';
@@ -23,7 +23,7 @@ export default class App extends Component<Props> {
   }
 }
 
-const Root = createStackNavigator(
+const Root = createMaterialTopTabNavigator(
   {
       Home: HomeScreen,
       Favorites: Favorites,
